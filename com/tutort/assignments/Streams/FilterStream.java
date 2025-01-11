33cfad16b45cc0ab7b4bff44f61f9586dac4333d
Bikash Jena <b2j1999jan27@gmail.com>
2025-01-11 20:51:17 +0530

    Streams...

--- com/tutort/assignments/Streams/FilterStream.java
+++ com/tutort/assignments/Streams/FilterStream.java
@@ -1,12 +1,15 @@
 package com.tutort.assignments.Streams;
 
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FilterStream {
     public static void main(String[] args) {
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        Stream<Integer> multipleoftwo = integerStream.filter((Integer i) -> i % 2 == 0);
+        // Explicit types
+        // (Integer i) -> i % 2 == 0
+        // Inferred types
+        // i -> i % 2 == 0
+        Stream<Integer> multipleoftwo = integerStream.filter(i -> i % 2 == 0);
         System.out.println(multipleoftwo.toList());
     }
 }
